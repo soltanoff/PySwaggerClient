489242dd390866f1d652e1e363ebad6a3bd4382d
soltanoff <piccadillable@gmail.com>
2023-10-11 18:12:53 +0300

    added missed requirements: `validate_email`

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open('requirements.txt') as f:
 
 setup(
     name='pyswaggerclient',
-    version='2.0.1',
+    version='2.0.2',
     packages=find_packages(
         exclude=('*.egg-info', 'build', 'dist', 'docs', 'deploy',),
     ),
