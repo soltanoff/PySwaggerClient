340b60a02ba316c470ea61753d0ad1db3466f07b
soltanoff <ilia@soltanov.dev>
2025-10-26 22:34:05 +0300

    Package version updates

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open('requirements.txt') as f:
 
 setup(
     name='pyswaggerclient',
-    version='2.3.0',
+    version='2.4.0',
     packages=find_packages(
         exclude=('*.egg-info', 'build', 'dist', 'docs', 'deploy',),
     ),
